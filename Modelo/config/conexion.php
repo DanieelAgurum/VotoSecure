a456884257d950cb5392f7b213c1e37f794f8611
brian esca <brianesca28@gmail.com>
2026-03-02 00:46:25 -0600

    Resuelto conflicto en conexion.php

--- Modelo/config/conexion.php
+++ Modelo/config/conexion.php
@@ -10,3 +10,25 @@ $conexion = new mysqli($host, $user, $password, $database);
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
 }
+class Conexion {
+
+    private $host = "localhost";
+    private $db = "votosecure";
+    private $user = "root";
+    private $pass = "";
+    private $charset = "utf8mb4";
+
+    public function conectar() {
+
+        $dsn = "mysql:host=$this->host;dbname=$this->db;charset=$this->charset";
+
+        try {
+            $pdo = new PDO($dsn, $this->user, $this->pass);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $pdo;
+        } catch (PDOException $e) {
+            die("Error de conexión");
+        }
+    }
+
+}
