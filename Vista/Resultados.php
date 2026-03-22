e70ea6815a63524c8e0b5795aa5cc28c0333cdb3
Anthony <cristo045millanperez@gmail.com>
2026-03-21 18:47:50 -0600

    .

--- Vista/Resultados.php
+++ Vista/Resultados.php
@@ -12,6 +12,7 @@ define('BASE_URL', '/VotoSecure');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="/votosecure/img/vs.ico">
     <title>Resultados Electorales - VotoSecure</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
