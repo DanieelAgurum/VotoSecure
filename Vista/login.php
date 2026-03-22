0d94b54cc7b04550682bcab5354dcd286439ef7e
Anthony <cristo045millanperez@gmail.com>
2026-03-22 12:53:19 -0600

    correcon de estilos css a tonalidades grises

--- Vista/login.php
+++ Vista/login.php
@@ -18,7 +18,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Iniciar Sesión - VotoSecure</title>
-
+    <link rel="icon" type="image/x-icon" href="/votosecure/img/vs.ico">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../css/login.css">
 </head>
@@ -46,7 +46,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
                         <input type="password" class="form-control" name="password" required>
                     </div>
 
-                    <button type="submit" class="btn btn-primary w-100">
+                    <button type="submit" class="btn btn-login w-100">
                         Iniciar Sesión
                     </button>
 
